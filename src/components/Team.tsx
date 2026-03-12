2358ef4616c38168ef02c7bf6759a8b018eb7ac4
SayanIndra83 <sayanindra0@gmail.com>
2026-03-13 02:16:33 +0530

    ..

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -44,7 +44,7 @@ const teamMembers: TeamMember[] = [
   {
     name: "Sayan Indra",
     post: "Tech Lead",
-    image: "/peoplePics/Sayan.jpeg",
+    image: "/peoplePics/Sayan2.jpeg",
     linkedin: "https://www.linkedin.com/in/sayan-indra-a41319369/",
     instagram: "https://www.instagram.com/sayanindra143/",
     x: "",
