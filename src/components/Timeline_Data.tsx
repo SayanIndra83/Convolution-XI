9b7b20451803e9939259348e5318f6e43bd0f9c9
SayanIndra83 <sayanindra0@gmail.com>
2026-03-12 18:26:23 +0530

    Timeline data updated

--- src/components/Timeline_Data.tsx
+++ src/components/Timeline_Data.tsx
@@ -3,36 +3,36 @@ export const events = [
     id: '01', 
     date: 'MARCH 27', 
     time: 'MORNING', 
-    items: ['Inauguration', 'Circuistics (Prelims)'] 
+    items: ['Inauguration', 'Inquizzitive (Prelims)'] 
   },
   { 
     id: '02', 
     date: 'MARCH 27', 
     time: 'AFTERNOON', 
-    items: ['Decisia (Finals)', 'Abol Tabol (Finals)'] 
+    items: ['Inquizzitive (Finals)' , 'Decisia (Finals)'] 
   },
   { 
     id: '03', 
     date: 'MARCH 28', 
     time: 'MORNING', 
-    items: ['Eureka (Prelims)','Inquizzitive (Prelims)', 'Sparkhack (Finals)'] 
+    items: ['Circuistics (Prelims)','Sparkhack (Finals)' ] 
   },
   { 
     id: '04', 
     date: 'MARCH 28', 
     time: 'AFTERNOON', 
-    items: ['Eureka (Round 2)' ,'Inquizzitive (Finals)'] 
+    items: ['Eureka! (Prelims)', 'Abol Tabol (Finals)'] 
   },
   { 
     id: '05', 
     date: 'MARCH 29', 
     time: 'MORNING', 
-    items: ['Circuistics (Finals)', 'Eureka (Finals)', 'Algomaniac (Finals)'] 
+    items: ['Circuistics (Finals)', 'Eureka! (Finals)'] 
   },
   { 
     id: '06', 
     date: 'MARCH 29', 
     time: 'AFTERNOON', 
-    items: ['JU Talks', 'Prize Distribution' ,'Closing Ceremony'] 
+    items: ['Algomaniac (Finals)','JU Talks', 'Prize Distribution' ,'Closing Ceremony'] 
   },
 ];
